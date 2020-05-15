4b90f1806bad826cbb854a1f00f49139e51f05aa
Alexis Sparrow <paltotrend@yandex.ru>
2020-05-15 15:21:21 +0300

    commit (15.05)

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,12 +1,38 @@
 import './about.css'
 
+
 var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-    spaceBetween: 16,
     direction: 'horizontal',
-    initialSlide:2,
-    slidesPerGroup: 2,
     loop: false,
+
+    breakpoints: {
+        320: {
+            spaceBetween:8,
+            initialSlide:0,
+            slidesPerView:1,
+            slidesPerGroup: 1,
+        },
+        768: {
+            initialSlide:2,
+            spaceBetween:8,
+            slidesPerView:1,
+            slidesPerGroup: 2,
+        },
+        769: {
+            initialSlide:2,
+            spaceBetween:8,
+            slidesPerView:2,
+            slidesPerGroup: 2,
+        },
+        1024: {
+            spaceBetween: 16,
+            initialSlide:2,
+            slidesPerView:1,
+            slidesPerGroup: 2,
+        }
+        
+    },
 
     // If we need pagination
     pagination: {
