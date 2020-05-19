c66582a1ad5e6d9b4393db1c7eea8c606f51c49a
Alexis Sparrow <paltotrend@yandex.ru>
2020-05-19 12:09:17 +0300

    commit (19.05)

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -1,5 +1,29 @@
-class NewsCard {
-    constructor() {
-        
+export default class NewsCard {
+    constructor(imageUrl, date, title, paragraph, source) {
+        this.imageUrl = imageUrl;
+        this.date = date;
+        this.title = title;
+        this.paragraph = paragraph;
+        this.source = source;
+    }
+
+    createCard() {
+        const card = document.createElement('div');
+        card.classList.add('cards__item');
+
+        const template = 
+            `<img class="cards__image" src="${this.imageUrl}" alt="Problem with connection or file">
+            <div class="cards__content-row">
+                <p class="cards__item-date">${this.date}</p>
+                <h2 class="cards__item-title">${this.title}</h2>
+                <div class="cards__item-paragraph">${this.paragraph}</div>
+                <p class="cards__item-source">${this.source}</p>
+            </div>`;
+
+        card.innerHTML = template;
+
+        console.log(card);
+
+        return card
     }
 }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,7 @@ import './index.css';
 
 import SearchForm from '../../js/components/SearchForm';
 import NewsApi from '../../js/modules/NewsApi';
+import NewsCardList from '../../js/components/NewsCardList';
 
 const newsApi = new NewsApi({
     baseUrl: 
@@ -10,8 +11,12 @@ const newsApi = new NewsApi({
         'from=2020-05-17&' +
         'sortBy=popularity&' +
         'apiKey=0d53a45fdb354aacb0b658c5523deca1',
-})
+    }, 
 
-const searchForm = new SearchForm(document.querySelector('.search__form'))
+);
 
-export {newsApi};
+const newsCardList = new NewsCardList(document.querySelector('.cards__container'), document.querySelector('.cards__root'), document.querySelector('.cards__search-container'), document.querySelector('.cards__error-container'),);
+
+const searchForm = new SearchForm(document.querySelector('.search__form'), document.querySelector('.cards__root'), document.querySelector('.cards__search-container'), document.querySelector('.cards'));
+
+export {newsApi, newsCardList};
